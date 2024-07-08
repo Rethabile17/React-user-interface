4a1bd888b218bd681c2fff6ca9f4761e87f08d57
peacendebele <peacendebele073@gmail.com>
2024-07-08 12:47:43 +0200

    Second

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,11 +1,14 @@
 import logo from "../images/logo.svg";
+import arrow from "../images/icon-arrow.svg"
+import left from "../images/icon-angle-left.svg"
+import right from "../images/icon-angle-right.svg"
 import "./navbar.css";
 function nav() {
   return (
     <div className="nav">
       <div className="navbar-container">
         <div class="nav-flex">
-          <img src={logo} alt="" />
+          <img class="room" src={logo} alt="" />
           <ul className="nav2">
             <li>home</li>
             <li>shop</li>
@@ -24,18 +27,16 @@ function nav() {
             collection and make your property a reflection of you and what you
             love.
           </p>
-          <h2>SHOP NOW </h2>
-          <div className="icon"></div>
-          <svg width="24" height="27" xmlns="http://www.w3.org/2000/svg">
-            <path
-              d="M1 0l12 12L1 24"
-              stroke="#FFF"
-              fill="none"
-              fill-rule="evenodd"
-            />
-          </svg>
+          <h2>SHOP NOW <span><img className="arrow" src={arrow}/></span></h2>
+        </div>
+        <div class="back-arrow">
+        <div className="arrow-img">
+          <img className="left" src={left}/>
+          <img className="right" src={right}/>
+          </div>
         </div>
       </div>
+     
     </div>
   );
 }
